1653a13ba78286b483ac7ff6adb39ce293526e29
Yonny Domingez <yonnydomingez22@gmail.com>
2023-01-31 10:01:27 -0500

    Todos los botones agregan el input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import './App.css';
+import freecodecamplogo from './Imgs/freecodecamp-logo.png';
+import Boton from './Components/Boton';
+import Pantalla from './Components/Pantalla';
+import BotonClear from './Components/BotonClear';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const addInput = val =>{
+    setInput(input + val);
+  };
+
   return (
-    <div className="App">
-      Hola      
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freecodecamplogo}
+          className='freecodecamp-logo'
+          alt='Logo de FreeCodeCamp' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input}/>
+        <div className='fila'>
+          <Boton manejarClic={addInput}>1</Boton>
+          <Boton manejarClic={addInput}>2</Boton>
+          <Boton manejarClic={addInput}>3</Boton>
+          <Boton manejarClic={addInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={addInput}>4</Boton>
+          <Boton manejarClic={addInput}>5</Boton>
+          <Boton manejarClic={addInput}>6</Boton>
+          <Boton manejarClic={addInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={addInput}>7</Boton>
+          <Boton manejarClic={addInput}>8</Boton>
+          <Boton manejarClic={addInput}>9</Boton>
+          <Boton manejarClic={addInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={addInput}>=</Boton>
+          <Boton manejarClic={addInput}>0</Boton>
+          <Boton manejarClic={addInput}>.</Boton>
+          <Boton manejarClic={addInput}>/</Boton></div>
+        <div className='fila'>
+          <BotonClear manejarClear={() => setInput('')}>
+            Clear
+          </BotonClear>
+        </div>
+      </div>     
     </div>
   );
 }
